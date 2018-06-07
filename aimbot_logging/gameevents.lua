bdc68f07f192e3f564d204dc506dbfdbff2f42f0
simpleavaster <sx12steelseries@gmail.com>
2018-06-07 00:57:18 -0600

    Made it easier to combine them all

--- aimbot_logging/gameevents.lua
+++ aimbot_logging/gameevents.lua
@@ -1,6 +1,28 @@
 local console_log = client.log
+local console_cmd = client.exec
+
+local is_enemy = client.is_enemy
+local is_local_player = client.is_local_player
+
+local get_player_name = client.get_player_name
+
+local read_entity_prop = client.read_entity_prop
+
+local userid_to_entindex = client.userid_to_entindex
+
+local cvar_get = client.cvar_get
+
+local draw_debug_text = client.draw_debug_text
+local draw_hitboxes = client.draw_hitboxes
+
+local RandomInt = client.RandomInt
+local RandomFloat = client.RandomFloat
+
+local tonumber = tonumber
 
 local hitgroup_names = { "body", "head", "chest", "stomach", "left arm", "right arm", "left leg", "right leg", "neck", "?", "body" }
+local hitbox_names = { "head", "neck", "pelvis", "stomach", "spine3", "spine2", "spine1", "left thigh", "right thigh", "left calf", "right calf", "left foot", "right foot", "left hand", "right hand", "left upperarm", "left forearm", "right upperarm", "right forearm", "all" }
+local group_to_hitboxes = { { 0 }, { 4, 5, 6 }, { 2, 3 }, { 15, 16 }, { 17, 18 }, { 7, 9, 11 }, { 8, 10, 12 }, { 1 } }
 
 local _M = {}
  
