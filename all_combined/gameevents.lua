04907216d4a726271f51993111004bfa338f0e6e
simpleavaster <sx12steelseries@gmail.com>
2018-06-08 01:59:02 -0600

    fixed

--- all_combined/gameevents.lua
+++ all_combined/gameevents.lua
@@ -1,24 +1,26 @@
 local console_log = client.log
 local console_cmd = client.exec
-
 local is_enemy = client.is_enemy
 local is_local_player = client.is_local_player
-
 local get_player_name = client.get_player_name
-
 local read_entity_prop = client.read_entity_prop
-
 local userid_to_entindex = client.userid_to_entindex
+local get_cvar = client.get_cvar
 
-local cvar_get = client.cvar_get
+local RandomInt = client.random_int
+local RandomFloat = client.random_float
 
+local worldtoscreen = client.world_to_screen
 local draw_debug_text = client.draw_debug_text
 local draw_hitboxes = client.draw_hitboxes
-
-local RandomInt = client.RandomInt
-local RandomFloat = client.RandomFloat
+local paint_draw_text = client.draw_text
+local paint_draw_rectangle = client.draw_rectangle
+local cheat_setvar = gs.set_var
+local cheat_getvar = gs.get_var
+local cheat_create_menu_checkbox = gs.create_menu_checkbox
 
 local tonumber = tonumber
+local math_floor = math.floor
 
 local hitgroup_names = { "body", "head", "chest", "stomach", "left arm", "right arm", "left leg", "right leg", "neck", "?", "body" }
 local hitbox_names = { "head", "neck", "pelvis", "stomach", "spine3", "spine2", "spine1", "left thigh", "right thigh", "left calf", "right calf", "left foot", "right foot", "left hand", "right hand", "left upperarm", "left forearm", "right upperarm", "right forearm", "all" }
@@ -37,7 +39,7 @@ bool    teleported     true if player is breaking lag compensation
 bool    high_priority  true if player is not using fake angles or is otherwise easier to hit
 ]]--
 function _M.aim_fire(e)
-    local group = hitgroup_names[e.hitgroup + 1] or "?"
+    local group = hitgroup_names[e.hitgroup] or "?"
     local hitchance = tonumber(string.format("%." .. 2 .. "f", e.hit_chance)) or 0 -- limit the precision of the float to 2 decimal places
 	local damage, backtrack, teleported, highpriority = e.damage, e.backtrack, e.teleported, e.high_priority
 
@@ -89,7 +91,7 @@ function _M.player_spawn(e)
     if userid == nil then return end
     if not is_local_player(userid) then return end 
 
-    local r_eyegloss, primary = cvar_get("r_eyegloss"), ''
+    local r_eyegloss, primary = get_cvar("r_eyegloss"), ''
     if r_eyegloss == nil then return end
 
     if r_eyegloss == "auto" then
--- autobuy/gameevents.lua
+++ autobuy/gameevents.lua
@@ -1,24 +1,26 @@
 local console_log = client.log
 local console_cmd = client.exec
-
 local is_enemy = client.is_enemy
 local is_local_player = client.is_local_player
-
 local get_player_name = client.get_player_name
-
 local read_entity_prop = client.read_entity_prop
-
 local userid_to_entindex = client.userid_to_entindex
+local get_cvar = client.get_cvar
 
-local cvar_get = client.cvar_get
+local RandomInt = client.random_int
+local RandomFloat = client.random_float
 
+local worldtoscreen = client.world_to_screen
 local draw_debug_text = client.draw_debug_text
 local draw_hitboxes = client.draw_hitboxes
-
-local RandomInt = client.RandomInt
-local RandomFloat = client.RandomFloat
+local paint_draw_text = client.draw_text
+local paint_draw_rectangle = client.draw_rectangle
+local cheat_setvar = gs.set_var
+local cheat_getvar = gs.get_var
+local cheat_create_menu_checkbox = gs.create_menu_checkbox
 
 local tonumber = tonumber
+local math_floor = math.floor
 
 local hitgroup_names = { "body", "head", "chest", "stomach", "left arm", "right arm", "left leg", "right leg", "neck", "?", "body" }
 local hitbox_names = { "head", "neck", "pelvis", "stomach", "spine3", "spine2", "spine1", "left thigh", "right thigh", "left calf", "right calf", "left foot", "right foot", "left hand", "right hand", "left upperarm", "left forearm", "right upperarm", "right forearm", "all" }
@@ -32,7 +34,7 @@ function _M.player_spawn(e)
     if userid == nil then return end
     if not is_local_player(userid) then return end 
 
-    local r_eyegloss, primary = cvar_get("r_eyegloss"), ''
+    local r_eyegloss, primary = get_cvar("r_eyegloss"), ''
     if r_eyegloss == nil then return end
 
     if r_eyegloss == "auto" then
