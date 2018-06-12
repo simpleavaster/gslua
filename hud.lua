7c0454c7efa85c6baa1ce11c4620d7f5919c24d7
simpleavaster <sx12steelseries@gmail.com>
2018-06-12 01:30:14 -0600

    added a hud, persistent killfeed (just basics)

--- hud.lua
+++ hud.lua
@@ -42,6 +42,36 @@ local math_floor = math.floor
 local table_insert = table.insert
 local table_remove = table.remove
 local string_format = string.format
+local string_replace = string.gsub
+
+----vector stuff
+local pi = 3.14159265358979323846
+local deg2rad = pi / 180.0
+local rad2deg = 180.0 / pi
+local cos, sin, atan, sqrt, min, max = math.cos, math.sin, math.atan, math.sqrt, math.min, math.max
+
+local function rotate_point(point_to_rot_x, point_to_rot_y, center_point_x, center_point_y, angle)
+    angle = angle * (deg2rad)
+    local cos_theta = cos(angle)
+    local sin_theta = sin(angle)
+    local x = cos_theta * (point_to_rot_x - center_point_x) - sin_theta * (point_to_rot_y - center_point_y)
+    local y = sin_theta * (point_to_rot_x - center_point_x) + cos_theta * (point_to_rot_y - center_point_y)
+    x = x + center_point_x
+    y = y + center_point_y
+    return x, y
+end
+
+local function get_length(x, y)
+    return sqrt(x^2 + y^2)
+end
+
+local function normalize(x, y)
+    local length = get_length(x, y)
+    norm_x = x / length
+    norm_y = y / length
+    return norm_x, norm_y
+end
+----
 
 local function draw_container(ctx, x, y, w, h)
     local c = {10, 60, 40, 40, 40, 60, 20}
@@ -50,11 +80,72 @@ local function draw_container(ctx, x, y, w, h)
     end
 end
 
+local function draw_radar(ctx)
+    local center_x, center_y = 400, 100
+    local local_player = entity_get_local_player()
+    local lp_x, lp_y, lp_z = entity_get_prop(local_player, "m_vecOrigin")
+    local lp_viewang_y = entity_get_prop(local_player, "m_angEyeAngles[1]")
+    local size = 150
+    draw_container(ctx, center_x-(size/2), center_y-(size/2), size, size)
+    
+    client_draw_rectangle(ctx, center_x, center_y, 5, 5, 255, 0, 0, 255)
+
+    local players = entity_get_players()
+    for i=1, #players do
+        local player = players[i]
+        local p_x, p_y, p_z = entity_get_prop(player, "m_vecOrigin")
+        p_x = lp_x - p_x
+        p_y = lp_y - p_y
+
+        local distance = min(get_length(p_x, p_y) * (0.02 * 1.5), 300)
+        local np_x, np_y = normalize(p_x, p_y)
+        np_x, np_y = np_x * distance, np_y * distance
+
+        np_x, np_y = np_x + center_x, np_y + center_y
+
+        local rnp_x, rnp_y = rotate_point(np_x, np_y, center_x, center_y, lp_viewang_y-90)
+
+        client_draw_rectangle(ctx, rnp_x, rnp_y, 5, 5, 255, 255, 255, 255)
+    end
+end
+
+local function sanitize_string(string)
+    return string:gsub("%W", "")
+end
+
 ---- CONFIG CONFIG CONFIG ----
 local only_show_money_in_buyzone = false
 ---- CONFIG CONFIG CONFIG ----
 
-local function on_paint_hud(ctx)
+local kills = {}
+local function on_player_death(e)
+    local victim, attacker, assister, weapon, headshot, dominated, revenge, penetrated = e.userid, e.attacker, e.assister, e.weapon, e.headshot, e.dominated, e.revenge, e.penetrated
+
+    if #kills >= 8 then
+        table_remove(kills, 1)
+    end
+
+    local killfeed = ''
+
+    local attacker_name = entity_get_player_name(client_userid_to_entindex(attacker))
+    attacker_name = sanitize_string(attacker_name)
+    local victim_name = entity_get_player_name(client_userid_to_entindex(victim))
+    victim_name = sanitize_string(victim_name)
+
+    if headshot then
+        killfeed = attacker_name .. " headshot " .. victim_name .. " with a " .. weapon
+    else
+        killfeed = attacker_name .. " killed " .. victim_name .. " with a " .. weapon
+    end
+
+    table_insert(kills, killfeed)
+end
+
+local function on_round_start(e)
+    kills = {}
+end
+
+local function drawhud(ctx)
     local scrsize_x, scrsize_y = client_screensize()
     local scrcenter_x, scrcenter_y = scrsize_x - (scrsize_x / 2), scrsize_y - (scrsize_y / 2)
     local local_player = entity_get_local_player() -- we use this multiple times, best to store it once instead of calling it multiple times
@@ -67,74 +158,58 @@ local function on_paint_hud(ctx)
     local c4_holder = entity_get_prop(playerresource, "m_iPlayerC4")
     local gameinfo = entity.get_all("CCSGameRulesProxy")
 
+    draw_radar(ctx)
+
     if armor > 1 and armor <= 100 then
         has_kevlar = true
     else
         has_kevlar = false
     end
 
-    ----bottom left hp armor
-    draw_container(ctx, -10, scrsize_y-45, 300, 55)
+    ----hp armor
+    local width = 80
+    draw_container(ctx, scrcenter_x-(width/2), scrsize_y-70, width, 80)
 
     if health >= 0 and health <= 100 then
-        client_draw_text(ctx, 25, scrsize_y-20, 255, 255, 255, 255, "c+", 0, "HP")
-        client_draw_text(ctx, 45, scrsize_y-33, 231, 76, 60, 255, "+", 0, health)
+        client_draw_text(ctx, scrcenter_x, scrsize_y-17, 231, 76, 60, 255, "c+", 0, health)
     end
 
-    if armor >= 100 then
-        client_draw_text(ctx, 180, scrsize_y-20, 255, 255, 255, 255, "c+", 0, "ARMOR")
-        client_draw_text(ctx, 230, scrsize_y-33, 52, 152, 219, 255, "+", 0, armor)
-    elseif armor >= 10 and armor < 100 then 
-        client_draw_text(ctx, 195, scrsize_y-20, 255, 255, 255, 255, "c+", 0, "ARMOR")
-        client_draw_text(ctx, 245, scrsize_y-33, 52, 152, 219, 255, "+", 0, armor)
-    elseif armor < 10 then
-        client_draw_text(ctx, 210, scrsize_y-20, 255, 255, 255, 255, "c+", 0, "ARMOR")
-        client_draw_text(ctx, 260, scrsize_y-33, 52, 152, 219, 255, "+", 0, armor)
+    if armor >= 0 and armor <= 100 then
+        client_draw_text(ctx, scrcenter_x, scrsize_y-43, 52, 152, 219, 255, "c+", 0, armor)
     end
     ----
+    ----armor flags
+    draw_container(ctx, scrcenter_x-200, scrsize_y-45, 68, 55)
 
-    ----flags
-    if teamnum == teamnum_ct then 
-        draw_container(ctx, 300, scrsize_y-45, 88, 55)
+    if has_helmet == 1 then 
+        client_draw_text(ctx, scrcenter_x-185, scrsize_y-33, 46, 204, 113, 255, "+", 0, "H")
+    elseif has_helmet == 0 then
+        client_draw_text(ctx, scrcenter_x-185, scrsize_y-33, 231, 76, 60, 255, "+", 0, "H")
+    end    
 
-        if has_helmet == 1 then 
-            client_draw_text(ctx, 315, scrsize_y-33, 46, 204, 113, 255, "+", 0, "H")
-        elseif has_helmet == 0 then
-            client_draw_text(ctx, 315, scrsize_y-33, 231, 76, 60, 255, "+", 0, "H")
-        end    
+    if has_kevlar == true then 
+        client_draw_text(ctx, scrcenter_x-165, scrsize_y-33, 46, 204, 113, 255, "+", 0, "K")
+    elseif has_kevlar == false then
+        client_draw_text(ctx, scrcenter_x-165, scrsize_y-33, 231, 76, 60, 255, "+", 0, "K")
+    end 
+    ----
 
-        if has_kevlar == true then 
-            client_draw_text(ctx, 335, scrsize_y-33, 46, 204, 113, 255, "+", 0, "K")
-        elseif has_kevlar == false then
-            client_draw_text(ctx, 335, scrsize_y-33, 231, 76, 60, 255, "+", 0, "K")
-        end 
+    ----defuser/c4 flags
+    draw_container(ctx, scrcenter_x-120, scrsize_y-45, 68, 55)
 
+    if teamnum == teamnum_ct then
         if has_defuser == 1 then 
-            client_draw_text(ctx, 355, scrsize_y-33, 46, 204, 113, 255, "+", 0, "D")
+            client_draw_text(ctx, scrcenter_x-94, scrsize_y-33, 46, 204, 113, 255, "+", 0, "D")
         elseif has_defuser == 0 then
-            client_draw_text(ctx, 355, scrsize_y-33, 231, 76, 60, 255, "+", 0, "D")
+            client_draw_text(ctx, scrcenter_x-94, scrsize_y-33, 231, 76, 60, 255, "+", 0, "D")
         end 
     elseif teamnum == teamnum_t then
-        draw_container(ctx, 300, scrsize_y-45, 101, 55)
-
-        if has_helmet == 1 then 
-            client_draw_text(ctx, 315, scrsize_y-33, 46, 204, 113, 255, "+", 0, "H")
-        elseif has_helmet == 0 then
-            client_draw_text(ctx, 315, scrsize_y-33, 231, 76, 60, 255, "+", 0, "H")
-        end    
-
-        if has_kevlar == true then 
-            client_draw_text(ctx, 335, scrsize_y-33, 46, 204, 113, 255, "+", 0, "K")
-        elseif has_kevlar == false then
-            client_draw_text(ctx, 335, scrsize_y-33, 231, 76, 60, 255, "+", 0, "K")
-        end 
-
         if c4_holder == local_player then 
-            client_draw_text(ctx, 355, scrsize_y-33, 46, 204, 113, 255, "+", 0, "C4")
+            client_draw_text(ctx, scrcenter_x-102, scrsize_y-33, 46, 204, 113, 255, "+", 0, "C4")
         else
-            client_draw_text(ctx, 355, scrsize_y-33, 231, 76, 60, 255, "+", 0, "C4")
-        end 
-    end    
+            client_draw_text(ctx, scrcenter_x-102, scrsize_y-33, 231, 76, 60, 255, "+", 0, "C4")
+        end
+    end
     ----
 
     ----money
@@ -171,6 +246,20 @@ local function on_paint_hud(ctx)
     end
     ----
 
+    ----killfeed
+    draw_container(ctx, scrsize_x-290, -10, 300, 185)
+
+    for i=1, #kills do
+        client_draw_text(ctx, scrsize_x-145, 19*i, 255, 255, 255, 255, "c", 0, kills[i])
+    end
+    ----
+end
+
+local origins = {}
+local function on_paint_hud(ctx)
+    drawhud(ctx)
 end
 
 client_set_event_callback("paint", on_paint_hud)
+client_set_event_callback("player_death", on_player_death)
+client_set_event_callback("round_start", on_round_start)
